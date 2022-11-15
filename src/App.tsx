33ad02dc0aa2f19d62a6b6bd43b4d2535202ac26
sadpu111 <dwme7100@gmail.com>
2022-11-15 23:42:52 +0900

    transfer location

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "./atoms";
 
 
 const Wrapper = styled.div`
@@ -32,28 +34,39 @@ const Card = styled.div`
   padding: 10px 10px;
   background-color: ${(props) => props.theme.cardColor};
 `;
-const toDos = ["a", "b", "c", "d", "e", "f"];
 
 function App() {
-  const onDragEnd = () => { };
+  const [toDos, setToDos] = useRecoilState(toDoState)
+  const onDragEnd = ({ draggableId, destination, source }: DropResult) => { // drag가 끝났을 때 실행되는 함수
+    if(!destination) return ; // 드래그가 끝나는 곳이 destination이 아니라면 그냥 놔둠
+    setToDos(preToDo => {
+      const copyToDo = [...preToDo];
+      copyToDo.splice(source.index, 1);
+      // 1) source.index 위치에서 1개 원소 삭제(드래그하는 원소 삭제)
+      copyToDo.splice(destination?.index, 0, draggableId)
+      // 2) destination.index 위치에 draggableId(toDo 내용) 추가(옮기는 위치에 드래그한 원소 추가)
+      return copyToDo
+    })
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-        <Droppable droppableId="one">
-          {(magic) => (
-            <Board ref={magic.innerRef} {...magic.droppableProps}>
-              {toDos.map((todo, index) => (<Draggable draggableId={todo} index={index}>
-                {(magic) => (
-                  <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>{todo}
-                  </Card>
-                )}
-              </Draggable>))}
-              {magic.placeholder}
-              {/* 드래그 할 때 사이즈 변화 x */}
-            </Board>
-          )}
-        </Droppable>
+          <Droppable droppableId="one">
+            {(magic) => (
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((todo, index) => (<Draggable key={todo} draggableId={todo} index={index}>
+                  {/* key와 draggableId가 같아야 함(beautiful dnd) */}
+                  {(magic) => (
+                    <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>{todo}
+                    </Card>
+                  )}
+                </Draggable>))}
+                {magic.placeholder}
+                {/* 드래그 할 때 사이즈 변화 x */}
+              </Board>
+            )}
+          </Droppable>
         </Boards>
       </Wrapper>
     </DragDropContext>
---
+++ src/atoms.tsx
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const toDoState = atom({
+key: "toDO",
+default: ["a", "b", "c", "d", "e", "f"],
+})
